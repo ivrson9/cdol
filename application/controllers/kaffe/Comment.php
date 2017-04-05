3a9b00a802d349ab496b86ef0fab9ca0ac24008a
ivrson9 <ivrson9@gmail.com>
2017-04-05 13:46:27 +0200

    sql

--- application/controllers/kaffe/Comment.php
+++ application/controllers/kaffe/Comment.php
@@ -16,14 +16,14 @@ class Comment {
 	}
 
 	function getList($con, $cafeNo){
-		$res = mysqli_query($con, "SELECT c.comment_no, c.id, c.comment, c.rating, c.comment_date, u.photo
+		$res = mysqli_query($con, "SELECT c.comment_no, u.name, c.comment, c.rating, c.comment_date, u.photo
 										FROM cafe_comment c join cafe_user u
-										WHERE c.cafe_no = ".$cafeNo." AND c.isDel = FALSE ORDER BY c.comment_date DESC");
+										WHERE c.cafe_no = ".$cafeNo." AND c.id = u.email AND c.isDel = FALSE ORDER BY c.comment_date DESC");
 
 		$result = array();
 
 		while($row = mysqli_fetch_array($res)){
-			array_push($result, array('comment_no'=>$row[0], 'id'=>$row[1], 'comment'=>$row[2], 'rating'=>$row[3], 'comment_date'=>$row[4], 'photo'=>$row[5]));
+			array_push($result, array('comment_no'=>$row[0], 'name'=>$row[1], 'comment'=>$row[2], 'rating'=>$row[3], 'comment_date'=>$row[4], 'photo'=>$row[5]));
 		}
 
 
